a1340d6776e26735ced2075332dffcd5c0e24b3c
fushuwei <331044761@qq.com>
2023-09-27 17:25:56 +0800

    解决GWT无法使用slf4j日志框架的问题

--- monaco-ui-samples/src/main/java/org/monacokit/monacoui/samples/AppEntryPoint.java
+++ monaco-ui-samples/src/main/java/org/monacokit/monacoui/samples/AppEntryPoint.java
@@ -1,6 +1,5 @@
 package org.monacokit.monacoui.samples;
 
-// import com.github.nalukit.nalu.plugin.elemental2.client.NaluPluginElemental2;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import elemental2.dom.DomGlobal;
@@ -15,18 +14,14 @@ import org.slf4j.LoggerFactory;
  */
 public class AppEntryPoint implements EntryPoint {
 
-    // private static final Logger logger = LoggerFactory.getLogger(AppEntryPoint.class);
+    private static final Logger logger = LoggerFactory.getLogger(AppEntryPoint.class);
 
     /**
      * This is the entry point method.
      */
     public void onModuleLoad() {
         // Initialize uncaught exception handler
-        // GWT.setUncaughtExceptionHandler(e -> logger.error("PANIC! -> UncaughtException Handler called", e));
-
-        // Create and start the application
-        // DataPlusApplication application = new DataPlusApplicationImpl();
-        // application.run(new NaluPluginElemental2());
+        GWT.setUncaughtExceptionHandler(e -> logger.error("PANIC! -> UncaughtException Handler called", e));
 
         // Cancel loading
         Element element = DomGlobal.document.getElementById("LoadingSpinner");
@@ -34,6 +29,6 @@ public class AppEntryPoint implements EntryPoint {
             element.remove();
         }
 
-        // logger.info("DataPlus UI have been initialized.");
+        logger.info("Monaco UI samples have been initialized.");
     }
 }
